e04ccaa7986a89eb8d952497864adb7872d63e3a
ayoubmkira <ayoub.mkira@gmail.com>
2021-07-01 09:44:30 +0100

    Initial Commit

---
+++ Script.js
@@ -0,0 +1,66 @@
+
+const btnNewMeal     = document.getElementById('btn');
+const imageElm       = document.getElementById('image');
+const ingredientsElm = document.getElementById('list-ingredients');
+const mealNameElm    = document.getElementById('meal-name');
+const instructionsElm = document.getElementById('instructions');
+
+// Return Object Of Every Ingredient And Its Measure:
+function filterIngredientsAndMeasures(meal) {
+
+	let list = {};
+	let entriesMeal = Object.entries(meal);
+	let ingredientsAndMesures = entriesMeal.filter(value => {
+		return value[0].startsWith('strIngredient') && value[1] !== '';
+	}).forEach((ingredient, idx) => {
+		let nameIngredient = ingredient[1];
+		let measureIngredient = meal[`strMeasure${idx + 1}`];
+		list[nameIngredient] = measureIngredient;
+	});
+
+	return list;
+
+}
+
+// Insert The Meal Into The Dom:
+function insertMealIntoDom(meal) {
+
+	const {
+		strInstructions: instructions,
+		strMealThumb: imageUrl,
+		strMeal: name
+	} = meal;
+	const ingredientsAndMeasures = filterIngredientsAndMeasures(meal);
+
+	imageElm.src = imageUrl;
+	ingredientsElm.innerHTML = '';
+	for(let prop in ingredientsAndMeasures) {
+		ingredientsElm.innerHTML += `
+			<li>
+				<span class="name">${prop}</span> - 
+				<span class="quantity">${ingredientsAndMeasures[prop]}</span>
+			</li>
+		`;
+	}
+	mealNameElm.innerText = name;
+	instructionsElm.innerText = instructions;
+
+}
+
+
+function generateNewMeal() {
+
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+	.then(response => response.json())
+	.then(data => data.meals[0])
+	.then(meal => {
+		insertMealIntoDom(meal);
+	})
+	.catch(error => console.log(error));
+
+}
+
+generateNewMeal();
+
+
+btnNewMeal.addEventListener('click', generateNewMeal);
